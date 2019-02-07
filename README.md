01c4d31becec0c093cc08f3e87e8495bbc79af9c
David Purdum <purdum41@gmail.com>
2019-02-07 17:31:03 -0500

    Add help to readme, create index page, update packages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HealthyHorizonsApp
+
+## How to Start Server
+
+Run `npm i` in the project folder.  Then run `node index.js` to start the server.
+
+Additionally, you can install `nodemon` which will allow the server to auto refresh the page on saved changes.  To do this, install it using `npm i nodemon -g` and then run the server using `nodemon index.js`.
--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var fs = require('fs')
 var ip = require('ip')
 
 var server = http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  if (q.pathname == "/") {
-	  filename = "./test.html"
-  }
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (q.pathname == "/") {
+        filename = "./index.html"
+    }
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
 })
 
 server.listen(8080);
