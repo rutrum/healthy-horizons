3c893e50270a9f2ca8f23cf961940175c06f2b71
Sam Badovinac <sbadovin@butler.edu>
2019-03-07 15:23:28 -0500

    Database connection and dbhandler stuff

--- DbHandler.js
+++ DbHandler.js
@@ -1,42 +1,32 @@
-var mysql = require('mysql')
 
+    var Connection = require('tedious').Connection
+    var Request = require('tedious').Request
 
+    var config = require('./dbCred.json')
 
-export function createConnection(){
-    
-}
+    var connection = new Connection(config)
 
-class DbHandler{
-    
-    //DbHandler constructor and connection
-    DbHandler(){
-        var con = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "password"
+    connection.on('connect', function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Server connection successful.")
+        }
+    })
+
+    exports.testRequest = function (callback) {
+        request = new Request("SELECT * FROM ITFacilities", function (err, rowCount) {
+            if (err) {
+                console.log(err)
+                callback("Internal Server Error")
+            }
         })
 
-        con.connect(function (err) {
-            if (err) throw err
-            console.log("Connected!")
+        // Upon completion of query, sends rows into callback function
+        request.on('doneInProc', function (rowCount, more, rows) {
+            callback(rows)
         })
-    }
 
-    //Function takes a SQL query and rerturns the result
-    Query(input){
-        con.connect(function (err) {
-            if (err) throw err
-            console.log("Connected!")
-            con.query(input, function (err, result) {
-                if (err) throw err
-                console.log("Result: " + result)
-            })
-        })
-    }
+        connection.execSql(request)
 
-    Login(){
-        var input = "Select email, password from User Where Email = @email AND Password = @Password";
-        DbHandler.Query(input)
-        
     }
-}
