e5411ffdb8c2c78714ff3fb65dfed1cd79252d82
David Purdum <purdum41@gmail.com>
2019-03-07 15:25:05 -0500

    Add tedious package

--- DbHandler.js
+++ DbHandler.js
@@ -1,32 +1,31 @@
+var Connection = require('tedious').Connection
+var Request = require('tedious').Request
 
-    var Connection = require('tedious').Connection
-    var Request = require('tedious').Request
+var config = require('./dbCred.json')
 
-    var config = require('./dbCred.json')
+var connection = new Connection(config)
 
-    var connection = new Connection(config)
+connection.on('connect', function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Server connection successful.")
+    }
+})
 
-    connection.on('connect', function (err) {
+exports.testRequest = function (callback) {
+    request = new Request("SELECT * FROM ITFacilities", function (err, rowCount) {
         if (err) {
             console.log(err)
-        } else {
-            console.log("Server connection successful.")
+            callback("Internal Server Error")
         }
     })
 
-    exports.testRequest = function (callback) {
-        request = new Request("SELECT * FROM ITFacilities", function (err, rowCount) {
-            if (err) {
-                console.log(err)
-                callback("Internal Server Error")
-            }
-        })
+    // Upon completion of query, sends rows into callback function
+    request.on('doneInProc', function (rowCount, more, rows) {
+        callback(rows)
+    })
 
-        // Upon completion of query, sends rows into callback function
-        request.on('doneInProc', function (rowCount, more, rows) {
-            callback(rows)
-        })
+    connection.execSql(request)
 
-        connection.execSql(request)
-
-    }
+}
