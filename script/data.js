6f4b4a29cc9ede747618b18362dd157d7aa08ed0
Sam Badovinac <sbadovin@butler.edu>
2019-04-11 15:06:45 -0400

    initialized data fields

--- script/data.js
+++ script/data.js
@@ -3,8 +3,24 @@ function getLocalFormData() {
     if (data == {}) {
         // No previous data.  Initialize new form data.
         oneweek = {
-            meditationClass = 0
-
+            meditationClass = 0,
+            fruits = 0,
+            noSugaryDrinks = 0,
+            fitnessClass = 0,
+            homeLunch = 0,
+            parkFar = 0,
+            sleep = 0,
+            upstairsBathroom = 0,
+            seatbelt = 0,
+            medications = 0,
+            snackForOffice = 0,
+            increasesteps = 0,
+            exercise = 0,
+            lunchAndLearn = 0,
+            read = 0,
+            healthyBlog = 0,
+            annualConsult = 0,
+            anualPhysical = 0
         }
     }
     return data;
