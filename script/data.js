37961c81cc596eb5569f5d9d57c62667c13b59f3
David Purdum <purdum41@gmail.com>
2019-04-16 14:43:41 -0400

    Populate fields with the data object

--- script/data.js
+++ script/data.js
@@ -1,26 +1,27 @@
 function getLocalFormData() {
-    const data = window.localStorage.getItem("formData");
-    if (data == {}) {
+    let data = window.localStorage.getItem("formData");
+    console.log(data)
+    if (data == null) {
         // No previous data.  Initialize new form data.
         oneweek = {
-            meditationClass = 0,
-            fruits = 0,
-            noSugaryDrinks = 0,
-            fitnessClass = 0,
-            homeLunch = 0,
-            parkFar = 0,
-            sleep = 0,
-            upstairsBathroom = 0,
-            seatbelt = 0,
-            medications = 0,
-            snackForOffice = 0,
-            increasesteps = 0,
-            exercise = 0,
-            lunchAndLearn = 0,
-            read = 0,
-            healthyBlog = 0,
-            annualConsult = 0,
-            anualPhysical = 0
+            meditationClass: 0,
+            fruits: 2,
+            noSugaryDrinks: 0,
+            fitnessClass: 0,
+            homeLunch: 0,
+            parkFar: 0,
+            sleep: 0,
+            upstairsBathroom: 0,
+            seatbelt: 0,
+            medications: 0,
+            snackForOffice: 0,
+            increasesteps: 0,
+            exercise: 0,
+            lunchAndLearn: 0,
+            read: 0,
+            healthyBlog: 0,
+            annualConsult: 0,
+            anualPhysical: 0
         }
         weeks = []
         for(let x = 0; x < 15; x++){
