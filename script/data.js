af010a16f84c1752a74df41390e6545d0f5341de
David Purdum <purdum41@gmail.com>
2019-04-18 14:56:57 -0400

    Some changes

--- script/data.js
+++ script/data.js
@@ -1,11 +1,11 @@
 function getLocalFormData() {
-    let data = window.localStorage.getItem("formData");
+    let data = JSON.parse(window.localStorage.getItem("formData"))
     console.log(data)
     if (data == null) {
         // No previous data.  Initialize new form data.
         oneweek = {
             meditationClass: 0,
-            fruits: 2,
+            fruits: 0,
             noSugaryDrinks: 0,
             fitnessClass: 0,
             homeLunch: 0,
@@ -15,17 +15,17 @@ function getLocalFormData() {
             seatbelt: 0,
             medications: 0,
             snackForOffice: 0,
-            increasesteps: 0,
+            increaseSteps: 0,
             exercise: 0,
             lunchAndLearn: 0,
             read: 0,
             healthyBlog: 0,
             annualConsult: 0,
-            anualPhysical: 0
+            annualPhysical: 0
         }
         weeks = []
         for(let x = 0; x < 15; x++){
-            weeks.push(oneweek)
+            weeks.push(JSON.parse(JSON.stringify(oneweek))) // deep copy hCAckkermamannn
         }
         data = weeks
     }
