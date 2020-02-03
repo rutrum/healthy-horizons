5cea98e099e0c68b58a6b5cf92e4c7fcdb188e2b
David Purdum <purdum41@gmail.com>
2020-02-03 16:09:57 -0500

    Add new database functions and new mysql dump

--- mysql_connection.js
+++ mysql_connection.js
@@ -1,21 +1,18 @@
+var mysql = require("mysql");
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'healthy.team',
+//   database : 'healthydb'
+// });
 
+// connection.connect();
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'HealthyHorizons',
-  password : 'HealthyHorizons',
-  database : 'mydb'
-});
-
-connection.connect();
-var datetime = new Date();
-
-//getTask(1); //TaskID
-//getTasks();
-getSizeofTasks(); 
+// getTask(1); //TaskID
+// getTasks();
+//getSizeofTasks(); 
 //getUserTasksByUser("'Earl'","'Gill'"); //First Name,Last Name
-insertAllTasksNewUser(3,datetime); // new user id, date  NEEDS getSizeofTasks to run but there are still issues
+//insertAllTasksNewUser(3,datetime); // new user id, date  NEEDS getSizeofTasks to run but there are still issues
 //getUserbyName("'Earl'","'Gill'"); //First Name,Last Name
 //getUserbyID(1);
 //getSizeofUsers(); 
@@ -23,17 +20,10 @@ insertAllTasksNewUser(3,datetime); // new user id, date  NEEDS getSizeofTasks to
 //updateUserTasks(1,1,"'2019-11-5'",1); //UserID,TaskID,date,frequency
 //getUserTasks();
 
-connection.end();
+// connection.end();
 
 function getTask(taskId){
   connection.query('SELECT * FROM tasks WHERE id = '+taskId, function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-  });
-};
-
-function getTasks(){
-  connection.query('SELECT * FROM tasks', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results);
   });
@@ -76,38 +66,12 @@ function insertAllTasksNewUser(users_id,date){
   };
 };
 
-function getUserbyID(userID){
-  connection.query('SELECT * FROM users WHERE id = '+userID, function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-  });
-};
-
 //cannot return usable datatype right now. same as getSizeofTasks() i believe
 function getSizeofUsers(){
   connection.query('SELECT COUNT(*) FROM users;', function (error, results, fields) {
     if (error) throw error;
     console.log(results[0]["COUNT(*)"]);
     return results[0]["COUNT(*)"];
-  });
-};
-
-function insertNewUser(email,f_name,l_name){
-  connection.query("INSERT INTO `mydb`.`users` (`email`, `f_name`, `l_name`)"+
-                    ' VALUES( '
-                    +email+
-                    ', '+f_name+
-                    ', '+l_name+')',  
-    function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-  }); 
-};
-
-function getUser(f_name,l_name){
-  connection.query('SELECT * FROM users WHERE f_name = '+f_name+' && l_name = '+l_name, function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
   });
 };
 
@@ -128,3 +92,87 @@ function getUserTasks(){
     console.log('The solution is: ', results);
   });
 };
+
+class HealthyDB {
+    constructor() {
+        this.connection = mysql.createConnection({
+            host     : 'localhost',
+            user     : 'root',
+            password : 'healthy.team',
+            database : 'healthydb'
+        })
+        this.connection.connect()
+    }
+
+    destroy() {
+        this.connection.end()
+    }
+
+    query_db(query, callback) {
+        this.connection.query(query, (error, results) => {
+            if (error) throw error
+            callback(results)
+        })
+    }
+
+    query_db_with_params(query, params, callback) {
+        this.connection.query(query, params, (error, results) => {
+            if (error) throw error
+            callback(results)
+        })
+    }
+
+    /// Task related functions
+
+    // Gets all tasks
+    all_tasks(callback) {
+        let q = "SELECT * FROM task;"
+        this.query_db(q, callback)
+    }
+
+    // Gets the task of the given id
+    task(id, callback) {
+        let q = "SELECT * FROM task WHERE id = ?;"
+        this.query_db_with_params(q, [id], callback)
+    }
+
+    // Adds the given task
+    add_task(task, callback) {
+        let q = "INSERT INTO task SET ?;"
+        this.query_db_with_params(q, task, callback)
+    }
+
+    /// User related functions
+
+    // Gets all users
+    all_users(callback) {
+        let q = `SELECT * from user;`
+        this.query_db(q, callback)
+    }
+
+    // Gets a user of the given id
+    user(id, callback) {
+        let q = `SELECT * FROM user WHERE id = ?;`
+        this.query_db_with_params(q, [id], callback)
+    }
+
+    // Adds the current user
+    add_user(user, callback) {
+        let q = "INSERT INTO user SET ?;"
+        this.query_db_with_params(q, user, callback)
+    }
+
+    /// Usertask related functions
+
+    usertasks(user_id, callback) {
+        let q = "SELECT * FROM usertask WHERE user_id = ?;"
+        this.query_db_with_params(q, [user_id], callback)
+    }
+}
+
+let db = new HealthyDB()
+db.user(4, (results) => {
+    console.log(results)
+})
+
+db.destroy()
